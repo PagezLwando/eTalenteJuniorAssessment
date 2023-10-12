37983500e37c909c2049576f9a1b234bde0dde7d
PagezLwando <lwandonodume@gmail.com>
2023-10-12 23:36:37 +0200

    Initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/lwandonodume/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lwandonodume.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/lwandonodume/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lwandonodume.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
